fb1cd5da34f6f3285bfe73d5785b1a10ba8fb7f1
kbojarowska <kbojarowska@wp.pl>
2021-11-29 14:28:21 +0100

    Zrobiono zadanie 2

---
+++ Zadanie2/SamplePythonProject/src/sample/FizzBuzz.py
@@ -0,0 +1,14 @@
+class FizzBuzz:
+    def game(self, num):
+        if type(num) is int: 
+            if num % 15 == 0:
+                return "FizzBuzz"
+            if num % 5 == 0:
+                return "Buzz"
+            if num % 3 == 0:
+                return "Fizz"
+            else:
+                return str(num)
+        else:
+            raise Exception("Error in FizzBuzz")
+            
